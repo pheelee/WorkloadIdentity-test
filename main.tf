f8fa8eac4d9029896abf87464173c465aa15e2b2
Philipp Ritter <2619327+pheelee@users.noreply.github.com>
2024-04-23 11:45:36 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ terraform {
 }
 
 provider "azuread" {
-  tenant_id     = "12345"
-  client_id     = "12345"
+  tenant_id     = "a5cbb983-25cb-4711-b3ef-10999b749045"
+  client_id     = "9a12052b-9897-4ddc-9f9b-94b67d2800e7"
   client_secret = "vtS8Q~UmzCbbwTDFnvbFTYMVv6CtV4M~2.veedBv"
 }
 
